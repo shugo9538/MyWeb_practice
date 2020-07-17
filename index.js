e67b81477dfcd247222d4d6b4717aeeb5d4ee963
shugo9538 <shugo9538@gmail.com>
2020-07-17 14:53:41 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let express = require('express');
+const ChromeLauncher = require('chrome-launcher');
+let app = express();
+let router = require('./router/main')(app);
+let port = 3000;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+
+let server = app.listen(port, function(){
+    console.log("Express server has started on port "+ port);
+    console.log("This Node.js is changed!");
+    console.log("second!")
+});
